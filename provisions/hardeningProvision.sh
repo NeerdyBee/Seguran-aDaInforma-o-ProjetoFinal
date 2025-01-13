de6ef4be921c4adf14fa88b8b5d061fe7aae98e9
NeerdyBee <116850945+NeerdyBee@users.noreply.github.com>
2025-01-13 19:38:30 -0300

    Update hardeningProvision.sh

--- provisions/hardeningProvision.sh
+++ provisions/hardeningProvision.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# -- Linhas de execução para instalação das ferramentas necessárias para o Hardening
+# -- Instalação das ferramentas necessárias para o Hardening
 apt-get update -y
 apt-get install -y iptables ssh curl wget xinetd
 
-# -- Hardening do IPTABLES
+# -- Configuração do firewall (iptables)
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT DROP
-
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
 iptables -A INPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT 
-iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT 
 iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT 
-
-# Cria arquivo com as regras atualizadas do IPTABLES
+iptables -A INPUT -i lo -j ACCEPT  
+iptables -A OUTPUT -o lo -j ACCEPT
 mkdir -p /etc/iptables
 touch /etc/iptables/rules.v4
 iptables-save > /etc/iptables/rules.v4
 
-# -- Hardening do Docker: Conatiner Web Apache
+# -- Configuração do Container Apache
 sudo docker exec -it web bash -c "
 apt-get update -y && apt-get install -y vim
 sed -i 's/Options Indexes/Options -Indexes/' /usr/local/apache2/conf/httpd.conf
@@ -32,10 +30,10 @@ echo '
 echo 'Header always set X-Content-Type-Options "nosniff"' >> /usr/local/apache2/conf/httpd.conf
 echo 'Header always set X-Frame-Options "DENY"' >> /usr/local/apache2/conf/httpd.conf
 "
-
 docker restart web
 
-# -- Hardening do XINETD: Apache
+# Hardening XINETD:
+# XINETD Apache
 echo 'includedir /etc/xinetd.d' >> /etc/xinetd.conf
 echo "service apache" >> /etc/xinetd.d/apache
 echo "{" >> /etc/xinetd.d/apache
@@ -46,7 +44,7 @@ echo "    server          = /usr/sbin/httpd" >> /etc/xinetd.d/apache
 echo "    server          = /usr/bin/docker server_args = exec -i web /usr/sbin/httpd -DFOREGROUND" >> /etc/xinetd.d/apache
 echo "    log_on_failure  += USERID" >> /etc/xinetd.d/apache
 echo "    log_on_success  += PID HOST DURATION" >> /etc/xinetd.d/apache
-echo "    only_from       = 192.168.1.0/24" >> /etc/xinetd.d/apache
+echo "    only_from       = 192.168.56.0/24" >> /etc/xinetd.d/apache
 echo "    no_access       = 0.0.0.0/0" >> /etc/xinetd.d/apache
 echo "    access_times    = 09:00-18:00" >> /etc/xinetd.d/apache
 echo "    port            = 80" >> /etc/xinetd.d/apache
@@ -62,7 +60,8 @@ echo "    rlimit_nofile   = 1024" >> /etc/xinetd.d/apache
 echo "    nice            = 10" >> /etc/xinetd.d/apache
 echo "}" >> /etc/xinetd.d/apache
 
-# -- Hardening do XINETD: SSH
+
+# XINETD SSH
 echo "service ssh" >> /etc/xinetd.d/ssh
 echo "{" >> /etc/xinetd.d/ssh
 echo "    disable         = no" >> /etc/xinetd.d/ssh
@@ -86,5 +85,4 @@ echo "    rlimit_stack    = 16M" >> /etc/xinetd.d/ssh
 echo "    rlimit_nofile   = 1024" >> /etc/xinetd.d/ssh
 echo "    nice            = 10" >> /etc/xinetd.d/ssh
 echo "}" >> /etc/xinetd.d/ssh
-
 service xinetd restart
